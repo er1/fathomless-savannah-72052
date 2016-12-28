0344e4c56199d3eb404ba0cb54e707501bc7eb31
Eric Chan <chanman_ca@yahoo.com>
2016-12-27 19:09:09 -0500

    added express js

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const http = require('http');
-http.createServer((request, response) => {
-	response.writeHead(200, {
-		'Content-Type': 'text/html'
-	});
-	response.end('<!DOCTYPE html><title>0</title>');
-}).listen(process.env.PORT || 8080);
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+	res.send('<!DOCTYPE html><title>0</title>');
+});
+
+app.get('/add/:a/:b', function (req, res) {
+	res.set('Content-Type', 'application/json');
+	res.send(JSON.stringify( { r: +req.params.a + +req.params.b } ));
+});
+
+app.get('/rnd', function (req, res) {
+	res.set('Content-Type', 'text/plain');
+	res.send((Math.random() < 0.5).toString());
+});
+
+app.get('/rnd/:n', function (req, res) {
+	res.set('Content-Type', 'text/plain');
+	res.send(Math.floor(1 + Math.random() * req.params.n).toString());
+});
+
+app.listen(process.env.PORT || 8080);
